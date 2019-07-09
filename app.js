d606b96bd6df99bf84b3193e19a91a1079d302f8
Denzel Archie <archie4denzel@gmail.com>
2019-07-09 17:19:09 -0500

    added youtube api

--- app.js
+++ app.js
@@ -1,16 +1,66 @@
 // clientID
-// const clientId = "n1ngtfb3jyknsdy5mdgl4zgqft4ekg";
+// const clientId = "n1ngtfb3jyknsdy5mdgl4zgqft4ekg"; Twitch
+// const apiKey = 'AIzaSyCDCMNwP9XUSHLgKEQ9j3ivsHfCcaJC9cQ';
+// const searchURL = 'https://www.googleapis.com/youtube/v3/search';
 
 ("use strict");
 // "Client-ID": "n1ngtfb3jyknsdy5mdgl4zgqft4ekg"
 const streamURL = "https://api.twitch.tv/helix/streams?language=en";
 const gameURL = "https://api.twitch.tv/helix/games/top";
+const searchURL = "https://www.googleapis.com/youtube/v3/search";
+const apiKey = "AIzaSyCDCMNwP9XUSHLgKEQ9j3ivsHfCcaJC9cQ";
+const clientId = "n1ngtfb3jyknsdy5mdgl4zgqft4ekg";
 
 function formatQueryParams(params) {
   const queryItems = Object.keys(params).map(
     key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`
   );
   return queryItems.join("&");
+}
+
+//Display Youtube video to DOM
+function displayYoutube(responseJson, maxResult) {
+  // if there are previous results, remove them
+  console.log(responseJson);
+  $("#results-list").empty();
+  // iterate through the json array, stopping at the max number of results
+  for (let i = 0; i < responseJson.items.length; i++) {
+    $("#results-list").append(
+      `<li><h3>${responseJson.items[i].snippet.title}</h3>
+      <p>${responseJson.items[i].snippet.description}</p>
+      <img src='${responseJson.items[i].snippet.thumbnails.default.url}'>
+      </li>`
+    );
+  }
+  //display the results section
+  $("#results").removeClass("hidden");
+}
+
+//Search Games using the Youtube API
+function getYouTubeVideos(query, maxResults = 10) {
+  const params = {
+    key: apiKey,
+    q: query,
+    part: "snippet",
+    maxResults,
+    type: "video"
+  };
+  const queryString = formatQueryParams(params);
+  const url = searchURL + "?" + queryString;
+
+  console.log(url);
+
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => displayYoutube(responseJson))
+    .catch(err => {
+      $("#js-error-message").text(`Something went wrong: ${err.message}`);
+    });
 }
 
 //Streams section
@@ -63,7 +113,7 @@ function getStreams(query, maxResults = 10) {
 
   const options = {
     headers: new Headers({
-      "Client-ID": "n1ngtfb3jyknsdy5mdgl4zgqft4ekg"
+      "Client-ID": clientId
     })
   };
 
@@ -93,7 +143,7 @@ function getGames(query, maxResults = 10) {
 
   const options = {
     headers: new Headers({
-      "Client-ID": "n1ngtfb3jyknsdy5mdgl4zgqft4ekg"
+      "Client-ID": clientId
     })
   };
 
@@ -145,6 +195,10 @@ function displayGames(responseJson, maxResult) {
 function watchForm() {
   $("form").submit(event => {
     event.preventDefault();
+    const searchTerm = $("#js-search-term").val();
+    const maxResults = $("#js-max-results").val();
+    getYouTubeVideos(searchTerm, maxResults);
+    $("search").click(getYouTubeVideos);
     $("#ranking").click(getGames);
     $("#online").click(getStreams);
   });
